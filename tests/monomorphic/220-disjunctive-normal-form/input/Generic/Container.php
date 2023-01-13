4b6bab2f04232ef31daed1074647fe1016a91c7a
Anton Sukhachev <mrsuh6@gmail.com>
2023-01-13 14:18:35 +0300

    php 8.2

--- tests/monomorphic/220-disjunctive-normal-form/input/Generic/Container.php
+++ tests/monomorphic/220-disjunctive-normal-form/input/Generic/Container.php
@@ -4,7 +4,7 @@ namespace Test\Generic;
 
 class Container<ContainerA,ContainerB,ContainerC>  {
 
-    private readonly (ContainerA&ContainerB)|ContainerC|null|false|true $content = null;
+    private readonly null|(ContainerA&ContainerB)|ContainerC|false|true $content = null;//@todo https://github.com/nikic/PHP-Parser/issues/910
 
     public function setContent((ContainerA&ContainerB)|ContainerC|null|false|true $content): (ContainerA&ContainerB)|ContainerC|null|false|true {
 
--- tests/monomorphic/220-disjunctive-normal-form/output/Generic/ContainerForTestEntityBirdAndTestEntityCatAndInt.php
+++ tests/monomorphic/220-disjunctive-normal-form/output/Generic/ContainerForTestEntityBirdAndTestEntityCatAndInt.php
@@ -4,7 +4,8 @@ namespace Test\Generic;
 
 class ContainerForTestEntityBirdAndTestEntityCatAndInt
 {
-    private readonly (\Test\Entity\Bird&\Test\Entity\Cat)|int|null|false|true $content = null;
+    private readonly null|(\Test\Entity\Bird&\Test\Entity\Cat)|int|false|true $content = null;
+    //@todo https://github.com/nikic/PHP-Parser/issues/910
     public function setContent((\Test\Entity\Bird&\Test\Entity\Cat)|int|null|false|true $content): (\Test\Entity\Bird&\Test\Entity\Cat)|int|null|false|true
     {
     }
